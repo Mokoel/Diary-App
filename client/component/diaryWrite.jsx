23ab438bc06a392bde83f001155d908533ed0bf6
paryuyu <107971288+paryuyu@users.noreply.github.com>
2022-10-06 16:05:30 +0900

    이미지피커, 유틸

--- client/component/diaryWrite.jsx
+++ client/component/diaryWrite.jsx
@@ -1,21 +1,30 @@
 import { useRoute } from "@react-navigation/native";
 import { Button, Image, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
-
-
+import ImagePicker from "./ImagePicker";
+import { imgStorageRegi } from "../util/diaryAPI";
 function DiaryWrite() {
     const route = useRoute();
     console.log(route.params)
+
+
+
+
     return ( 
         <>
         <View style={styles.container}>
-
+ 
         <Text>글쓰는 공간{route.params}</Text>
+
+
             <Text style={{fontSize:20,marginBottom:10}}>😊😁😍😒🤩</Text>
             <TextInput placeholder={"글을 입력해주세요!."} style={{width:"90%",fontFamily:"GamjaFlower",fontSize:20,borderWidth:1,borderRadius:5,paddingLeft:10,height:200,marginBottom:30}}/>
+
+
             </View>
             
             <View style={{flex:1}}>
+
             <Image source={require("../assets/clouds.jpg")} style={{width:180,height:180,marginLeft:"5%"}}/>
             <Pressable  android_ripple={{color:"lightblue",borderless:true,}}>
             <View style={{alignItems:"flex-end"}}>
@@ -23,6 +32,8 @@ function DiaryWrite() {
             </View>
             </Pressable>
             </View>
+
+
             <Button title="입력"></Button>
         </>
      );
