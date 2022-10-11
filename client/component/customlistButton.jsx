74cc0b81b3674a84a9a4630090897f2416a1738b
paryuyu <107971288+paryuyu@users.noreply.github.com>
2022-10-11 17:02:31 +0900

    리스트 스타일

---
+++ client/component/customlistButton.jsx
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from "react-native";
+
+
+
+function CustomListButton({ children }) {
+    return (<View style={styles.buttonBox}><Text style={styles.button}>{children}</Text></View>);
+}
+
+
+const styles = StyleSheet.create({
+buttonBox:{
+    backgroundColor:"#d0d0d0",
+    paddingVertical:3,
+    paddingHorizontal:5,
+    borderRadius:6,
+    alignItems:"center",
+    marginHorizontal:2
+},
+button:{
+    color:"#333",
+   // fontWeight:"bold",
+    fontSize:12
+
+}
+});
+
+
+export default CustomListButton;
