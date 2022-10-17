590cc1966e6a0203755edf3ef108aeedf0279445
paryuyu <107971288+paryuyu@users.noreply.github.com>
2022-10-17 09:41:06 +0900

    캘린더 마진탑 수정

--- client/screens/calendarView.jsx
+++ client/screens/calendarView.jsx
@@ -182,9 +182,8 @@ function CalendarView() {
 
 const styles = StyleSheet.create({
   calendar: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#e0e0e0',
-    height: "80%"
+    height: "80%",
+    marginTop:100
   },
   centeredView: {
     flex: 1,
